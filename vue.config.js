1e049b1b7b89347a75d8bf0b110a21b51f806468
IH-Wang <ih.wang49@gmail.com>
2021-06-16 15:00:18 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  lintOnSave: true,
   publicPath:
     process.env.NODE_ENV === 'production' ? '/actions-example-vue/' : '/',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = process.env['VUE_APP_TITLE'] || 'CI /CD 測試 demo';
+      return args;
+    });
+  },
+  productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
 };
